ebf42e4e176675d127611aebbcc0aedf72dfd081
Stefan Hagspiel <shagspiel@dachcom.ch>
2025-12-09 10:33:00 +0100

    refactor PHP-CS-Fixer setup: separate finder configuration and adjust workflow inputs

---
+++ .php-cs-fixer-finder.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+return PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+
+    ->exclude([
+        __DIR__ . '/tests/_output',
+        __DIR__ . '/tests/Support/_generated',
+    ])
+;
