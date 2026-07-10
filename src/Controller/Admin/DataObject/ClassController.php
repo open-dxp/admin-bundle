634d5fb1d64b2c1ee54ed8f6c20522eb5342ef05
Niklas <niklas.brunberg@cag.se>
2026-07-10 13:55:56 +0200

    fix: add guards for read/get operations in controllers and assets (#94)

--- src/Controller/Admin/DataObject/ClassController.php
+++ src/Controller/Admin/DataObject/ClassController.php
@@ -1820,6 +1820,9 @@ class ClassController extends AdminAbstractController implements KernelControlle
         for ($i = $startIndex; $i < $limit; $i++) {
             $icon = $icons[$i];
             $content = file_get_contents(OPENDXP_WEB_ROOT . $icon);
+            if ($content === false) {
+                continue;
+            }
             $result[] = [
                 'text' => sprintf(
                     '<img style="%s" src="data:%s;base64,%s"/>',
