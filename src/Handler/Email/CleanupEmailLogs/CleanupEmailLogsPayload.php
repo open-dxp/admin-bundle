b0292c2609ebc64df30b0c88fac102f2f34af76c
Stefan Hagspiel <shagspiel@dachcom.ch>
2026-08-18 13:50:25 +0200

    add email log cleanup to the global email log panel (#97)

---
+++ src/Handler/Email/CleanupEmailLogs/CleanupEmailLogsPayload.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * OpenDXP
+ *
+ * This source file is licensed under the GNU General Public License version 3 (GPLv3).
+ *
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) OpenDXP (https://www.opendxp.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html  GNU General Public License version 3 (GPLv3)
+ */
+
+namespace OpenDxp\Bundle\AdminBundle\Handler\Email\CleanupEmailLogs;
+
+use OpenDxp\Bundle\AdminBundle\Payload\ExtJsPayloadInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+final readonly class CleanupEmailLogsPayload implements ExtJsPayloadInterface
+{
+    public const int MINIMUM_DAYS = 1;
+
+    public function __construct(
+        public readonly int $olderThanDays,
+    ) {
+    }
+
+    public static function fromRequest(Request $request): static
+    {
+        return new static(
+            olderThanDays: (int) $request->request->getString('olderThanDays'),
+        );
+    }
+}
